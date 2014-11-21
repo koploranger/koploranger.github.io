f9062b429496f835f74eded7f5ea4e4a0218e52c
achmad saiful <achmads123@gmail>
2014-11-21 16:48:51 +0700

    no message

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+       
+    <title>Koploranger</title>
+      
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/scrolling-nav.js"></script>
+
+    </head>
+    <body>
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Koploranger</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active" ><a href="#slide-1" class="page-scroll">Home</a></li>
+              <li><a href="#slide-2" class="page-scroll">About</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Meet The People <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#slide-3" class="page-scroll">Ipul</a></li>
+                  <li><a href="#slide-4"class="page-scroll">Zaki</a></li>
+                  <li><a href="#slide-5"class="page-scroll">Dita</a></li>
+                  <li><a href="#slide-6"class="page-scroll">Niswah</a></li>
+                  <li><a href="#slide-7"class="page-scroll">Boim</a></li>
+                  <li><a href="#slide-8"class="page-scroll">Yoga</a></li>
+                </ul>
+              </li>
+              <li><a href="#footer"class="page-scroll">Contact Me</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </nav>
+      
+      <section id="slide-1">
+        <div class="bcg">
+          <div class="row">
+          </div>
+        </div>
+      </section>
+      <section id="slide-2">
+        <div class="bcg">
+        </div>
+      </section>
+      <section id="slide-3">
+        <div class="bcg">
+          <img class="fotoholder" src="img/ipul.jpg" height="200" width="200">
+          <div class="nameholder">Achmad Saiful</div>
+          <div class="desholder">
+            Orang tertua di Koplo Rangers. Walaupun tidak pernah diadakan pemilu, secara aklamasi koplorangers menganggap ipul sebagai leader kami. Soalnya bisa dibilang ipul itu orang paling dewasa di koplorangers. Ipul, orang paling fleksibel kalo diajak kemana-mana, apalagi kalo dimintai tolong buat nebeng . Ipul itu kadang provokator, apalagi kalo ada adik kelas yg mlete. Dan entah kenapa, ipul itu orang yang paling sering mendengar curhat galau dari koplorangers, mungkin dia emang yg paling dewasa di koplorangers.
+          </div>
+          <div class="iconholder">
+            <a href="http://facebook.com/achmads23" target="_blank">
+              <img src="img/facebook.jpg" height="50" width="50">
+            </a>
+            <a href="http://twitter.com/achmads23" target="_blank">
+              <img src="img/twitter.jpg" height="50" width="50">
+            </a>
+          </div>
+        </div>
+      </section>
+      <section id="slide-4">
+        <div class="bcg">
+          <img class="fotoholder" src="img/zaki.jpg" height="200" width="200">
+          <div class="nameholder">Ismail Zaky Al-Fatih</div>
+          <div class="desholder">
+            Pak ustadnya koplorangers nih ! Orang ter-unique di Koplo Rangers. Kenapa unik ? Soalnya dia itu suka manjat-manjat kayak monyet, mungkin punya obsesi jadi atlet parkour tapi ngga kesampaian :p Zaki itu juga sering banget tidur di kelas, duduk di depan guru pun dia tidur -.-  Walaupun pencilakan, Zaki itu paling alim di koplorangers. Selain itu, dia juga punya bakat di sastra. Dan yang terakhir, dia itu yang paling banyak fans nya di Koplorangers, sekarang zaki susah dihubungi entah kenapa hanya dita yang tau kabarnya :p, jarang sekali ngumpul si zaki, andaikan dia tau, kami merindukanmu tsaaah ~~ .
+          </div>
+          <div class="iconholder">
+            <a href="http://facebook.com/Ismail.Zaky.Al.Fatih" target="_blank">
+              <img src="img/facebook.jpg" height="50" width="50">
+            </a>
+            <a href="http://twitter.com/ismailzaki" target="_blank">
+              <img src="img/twitter.jpg" height="50" width="50">
+            </a>
+          </div>
+        </div>
+      </section>
+      <section id="slide-5">
+        <div class="bcg">
+          <img class="fotoholder" src="img/dita.jpg" height="200" width="200">
+          <div class="nameholder">Rahmaditta Fillaili</div>
+          <div class="desholder">
+            Rahmaditta Fillaili, biasa dipanggil dita. dari KTPnya menunjukkan cewek, tetapi jika anda kenal lebih jauh dia bukanlah cewek biasa, tomboy ! gak pernah bisa diem tapi kalo udah ketemu orang yang dia sukain pasti langsung beda 180 derajat. langsung diem dan jaim. diantara kita, dita lah yang paling jaim. yang paling menyenangkan dari dita ini adalah ketika makan, makannya dikit jaim jaim entah gimana, tapi bayarnya FULL. dan ada lagi, kalau dita bawa makanan coba minta dikit biasanya dikasihin semuanya :p.
+          </div>
+          <div class="iconholder">
+          <a href="untukdita.php">
+              <img src="img/gift.png" height="57" width="57">
+            </a>
+            <a href="http://facebook.com/rahmadita.filaili" target="_blank">
+              <img src="img/facebook.jpg" height="50" width="50">
+            </a>
+            <a href="http://twitter.com/dittditta" target="_blank">
+              <img src="img/twitter.jpg" height="50" width="50">
+            </a>
+          </div>
+        </div>
+      </section>
+      <section id="slide-6">
+        <div class="bcg">
+          <img class="fotoholder" src="img/niswah.jpg" height="300" width="300">
+          <div class="nameholder">Niswah Qurrotul Aini</div>
+          <div class="desholder">
+            Orang tergalau di Koplo Rangers. Teman teman manggil niswah itu niswah, kalau dita manggil kem, kependekan dari kembaran soalnya dulu kita kayak orang kembar tapi beda, tapi bagi ipul kem itu kemplo, entahlah pul ~ . Niswah itu orang paling aktivis di Koplo Rangers, secara waktu SMA dia ikut OSIS sama SKI sekaligus, di Kuliahpun begitu, pernah saat kami ke malang dia masih sibuk dengan kegiatan kampusnya, katanya sih dengan kesibukan niswah bisa melupakan galaunya, "katanya :P". Niswah, walaupun badannya kecil tapi makannya gila-gila an-bisa dibilang dia melihara anakonda di perutnya. Niswah punya mimpi buat jadi Menteri Ekonomi di Indonesia (semoga tercapai yaa, kem . Amin Ya Rabb).
+          </div>
+          <div class="iconholder">
+            <a href="http://facebook.com/niswah.aini" target="_blank">
+              <img src="img/facebook.jpg" height="50" width="50">
+            </a>
+            <a href="http://twitter.com/adaniswah" target="_blank">
+              <img src="img/twitter.jpg" height="50" width="50">
+            </a>
+          </div>
+        </div>
+      </section>
+      <section id="slide-7">
+        <div class="bcg">
+          <img class="fotoholder" src="img/boim.jpg"  height="200" width="200">
+          <div class="nameholder">Muhammad Ibrahim</div>
+          <div class="desholder">
+            Orang ter-santai di Koplorangers. Dia sering dipanggil 'mbah'. Awal mula dia dipanggil mbah itu gara-gara ipul :p ("ya dia metuek banget sih" haha :p). Dia emang cocok kok dipanggil mbah, soalnya sifatnya dia yang klasik banget. Boim penggila seni. Waktu sma dia ambil ekskul teater sih emang ._. Walaupun klasik, tapi dia juga sering banget baca buku-buku jaman sekarang. haha <BR>HOT NEWS !! BOIM MAU NYALON JADI KETUA DI ORGANISASI TEMPAT DIA BERNAUNG, tunggu aja rilis websitenya di halaman ini :3
+          </div>
+          <div class="iconholder">
+            <a href="http://facebook.com/mbah.m.ibrahim" target="_blank">
+              <img src="img/facebook.jpg" height="50" width="50">
+            </a>
+            <a href="http://twitter.com/mbahboim" target="_blank">
+              <img src="img/twitter.jpg" height="50" width="50">
+            </a>
+          </div>
+        </div>
+      </section>
+      <section id="slide-8">
+        <div class="bcg">
+          <img class="fotoholder" src="img/yoga.jpg" height="200" width="300">
+          <div class="nameholder">Yoga Aditia</div>
+          <div class="desholder">
+             Orang ter-maaf ya yog-childish. Yoga itu sifatnya masih ke kanak-kanak an. Ngga suka dikritik orang lain tapi masih sering ngeluh (sudah mahasiswa, yog, ayo berubah ! :D ). Yoga juga paling sering di'bully' sama Koplorangers yang lain (maaf ya, yog. hehe ._.V ). Meskipun begitu, yoga itu orangnya baik kok kalo diajak berteman :D. btw yoga mau lulus loh :D, ga sabar buat dateng di wisudanya yoga nih :D. cepet lulus ya yog, doakan kami cepat lulus jugaaaa :D.
+          </div>
+          <div class="iconholder">
+            <a href="http://facebook.com/yoga.aditia.3" target="_blank">
+              <img src="img/facebook.jpg" height="50" width="50">
+            </a>
+            <a href="http://twitter.com/yoga_aditian" target="_blank">
+              <img src="img/twitter.jpg" height="50" width="50">
+            </a>
+          </div>
+        </div>
+      </section>
+      <section id="slide-9">
+        <div class="bcg">
+          
+        </div>
+      </section>
+      <div id="footer">
+        
+      </div>
+
+      
+    </body>
+  </head>
+</html>
